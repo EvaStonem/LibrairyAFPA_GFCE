9d7494df59518d8933a530302b7bac9c3898375c
Randria Jeff <randria_francis@yahoo.fr>
2018-02-26 15:58:14 +0100

    je test aussi

--- src/java/Servlets/controller.java
+++ src/java/Servlets/controller.java
@@ -37,7 +37,7 @@ public class controller extends HttpServlet {
             //Pas d'affichage
             //Creer les URL 
             //Creer les Sessions
-            //je test Randria
+            //je commence ici
         
     }
 
